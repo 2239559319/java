de389068fbafde84363c6be4ac303c65d8c00cc6
2239559319 <2017141461249@stu.scu.edu.cn>
2018-09-17 12:35:17 +0800

    输入输出

---
+++ 基础/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("输入一个整数");
+        int a=input.nextInt();
+        System.out.println(a);
+    }
+}
