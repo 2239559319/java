d744878ee827012b0a0892c15926af799788a2a5
2239559319 <2017141461249@stu.scu.edu.cn>
2018-09-18 12:02:23 +0800

    随机数处理

---
+++ 基础/Random.java
@@ -0,0 +1,6 @@
+public class Random {
+    public static void main(String[] args){
+        System.out.println(Math.random());          //返回0.0到1.0之间的一个随机数
+        System.out.println((int)(Math.random()*10));//返回1到10之间的一个随机数整数
+    }
+}
